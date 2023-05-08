f89e9b87e73657088ab6007c7e5035bab73f20b6
guilhermefonseca1 <guilherme.limasoares@hotmail.com>
2023-05-08 18:36:46 -0300

    Criando tela de Cadastro de clientes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto_Controle_vendas.br.com.projeto.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Projeto_Controle_vendas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frmclientes());
         }
     }
 }
