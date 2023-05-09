5bd268f2b40ca78753f478bdbf414b8ee730f414
guilhermefonseca1 <guilherme.limasoares@hotmail.com>
2023-05-08 23:12:22 -0300

    Configurando variável de conexão com o banco de dados

--- br.com.projeto.conexao/ConnectionFactory.cs
+++ br.com.projeto.conexao/ConnectionFactory.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projeto_Controle_vendas.br.com.projeto.conexao
 {
-    internal class ConnectionFactory
+    public class ConnectionFactory
     {
+        public MySqlConnection getConnection()
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["bdvendas"].ConnectionString;
+            
+            return  new MySqlConnection(conexao);
+        }
     }
 }
