f69fa27d215f0bdd5c73df931619daf01a1ff1f3
guilhermefonseca1 <guilherme.limasoares@hotmail.com>
2023-05-08 21:48:21 -0300

    Configurando as classes de conexão com o banco de dados

---
+++ br.com.projeto.conexao/ConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Controle_vendas.br.com.projeto.conexao
+{
+    internal class ConnectionFactory
+    {
+    }
+}
